4119ee966317e92f3b9c953f7bbb50e28dd792de
teo3300 <teo3300>
2025-12-11 19:14:11 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# What?!
+
+Yes, you can now write C in latin
+
+If anyone is more knowledgeable than me in latin, please suggest improvements
+and I will make new versions of the file (to not break compatibility no file
+will be removed and a new file will be added everytime.
---
+++ latinum_00.h
@@ -0,0 +1,76 @@
+#ifndef C_KEYWORDS_LATINUM_H
+#define C_KEYWORDS_LATINUM_H
+
+// Mapping C keywords to Latin (and some creative translations)
+#define numerus int
+#define fluitantis float
+#define duplici double
+#define littera char
+#define si if
+#define alioquin else
+#define remitte return
+#define vacuus void
+#define pro for
+#define quamdiu while
+#define desine break
+#define pergit continue
+#define quale switch
+#define est case
+#define norma default
+#define structura struct
+#define unionorma union
+#define renominare typedef
+#define genera enum
+#define constans const
+#define magnitudo sizeof
+#define staticus static
+#define peregrinus extern
+#define registrum register
+#define intralineam inline
+
+// Additional C keywords
+#define automaticus auto
+#define volatile volatile
+#define alignare alignas
+#define aptatur alignof
+#define sola atomic
+#define bina bool
+#define complexus complex
+#define imaginarius imaginary
+#define limitatus restrict
+#define genus typeof
+#define congregatus asm
+#define capere catch
+#define classis class
+#define dele delete
+#define conversio_dynamica dynamic_cast
+#define palam explicit
+#define exportare export
+#define amicus friend
+#define mutabilis mutable
+#define nomen namespace
+#define novum new
+#define exeptiones_non_permittit noexcept
+#define substitutere override
+#define privatum private
+#define protectum protected
+#define publicus public
+#define reinterpretatur_ut reinterpret_cast
+#define certum_statica static_assert
+#define conversio_statica static_cast
+#define exemplum template
+#define hoc this
+#define emitte throw
+#define conare try
+#define identificator_generis typeid
+#define nomen_generis typename
+#define utens using
+#define virtualis virtual
+#define littera_extensa wchar_t
+
+// Other
+#define certum assert
+#define verum true
+#define falsus false
+
+#endif
